827c4974c27062a250dd68a258503998515980b1
amanycalazans <amanycalazans@gmail.com>
2023-01-31 18:37:18 -0300

    desafio for sem usar números

---
+++ src/main/java/b_controle/DesafiaFor.java
@@ -0,0 +1,10 @@
+package b_controle;
+
+public class DesafiaFor {
+    public static void main(String[] args) {
+
+        for(String v = "#"; !v.equals("######"); v+= "#"){
+            System.out.println(v);
+        }
+    }
+}
