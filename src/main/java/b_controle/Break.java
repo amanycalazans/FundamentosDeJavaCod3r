29a6657ac550cd80c653eb6cd1b6ff3f68b89b2d
amanycalazans <amanycalazans@gmail.com>
2023-02-07 20:29:24 -0300

    Break & Continue

---
+++ src/main/java/b_controle/Break.java
@@ -0,0 +1,13 @@
+package b_controle;
+
+public class Break {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 10; i++){
+            if(i == 5)
+                break;
+            System.out.println(i);
+        }
+
+    }
+}
